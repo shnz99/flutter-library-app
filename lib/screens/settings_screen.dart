f1337e1b5eae62db900354657a03f7997759424b
Damian Gawin <dg_shnz99@pm.me>
2025-01-08 21:08:52 +0100

    Fix required parameter issues in BookDetailsScreen and BookListItem (#11)
    Fix missing required parameters and import statements.
    
    * **lib/main.dart**
    - Provide the required 'book' parameter in the `BookDetailsScreen`
    constructor.
      - Use a sample `Book` instance for the 'book' parameter.
    
    * **lib/widgets/book_list_item.dart**
    - Add the 'required' modifier to the 'book' parameter in the
    `BookListItem` constructor.
    
    * **lib/screens/settings_screen.dart**
    - Import the `BlockPicker` class from the `flutter_colorpicker` package.

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
